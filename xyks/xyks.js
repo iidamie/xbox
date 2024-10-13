971f5f81328f3323501872c0899253bde6ad3ee5
iidamie <127737368+iidamie@users.noreply.github.com>
2024-10-14 00:54:42 +0800

    更新 xyks.js

--- xyks/xyks.js
+++ xyks/xyks.js
@@ -4,7 +4,7 @@ const scriptName = "小猿口算pk";
 let responseBody = $response.body;
 
 // 使用正则表达式替换 URL
-responseBody = responseBody.replace(/https?:\/\/leo\.fbcontent\.cn\/bh5\/leo-web-oral-pk\/exercise[^\/]+\.js/g, 'https://cdn.rawgit.com/iidamie/xbox/xyks/main/exercise.js');
+responseBody = responseBody.replace(/https?:\/\/leo\.fbcontent\.cn\/bh5\/leo-web-oral-pk\/exercise[^\/]+\.js/g, 'https://cdn.rawgit.com/iidamie/xbox/xyks/main/execise.js');
 
 // 返回修改后的响应体
 $response.body = responseBody;
